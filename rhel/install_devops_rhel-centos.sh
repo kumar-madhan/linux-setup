9370e91164e5ac90c08ddac5510e25d163dfb8c4
kumar-madhan <madhankumar150598@gmail.com>
2024-05-15 10:18:11 +0530

    .

--- rhel/install_devops_rhel-centos.sh
+++ rhel/install_devops_rhel-centos.sh
@@ -2,59 +2,55 @@
 
 install_git() {
     echo "Installing Git..."
-    # Add commands to install Git here
     sudo yum install -y git
 }
 
 install_ansible() {
-    echo "Installing Git..."
-    # Add commands to install Git here
-    sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+    echo "Installing Ansible..."
+    sudo yum install -y epel-release
     sudo yum install -y ansible
-
 }
 
 install_jenkins() {
     echo "Installing Jenkins..."
-    # Add commands to install Git here
     sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
     sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     sudo yum upgrade -y
-    # Add required dependencies for the jenkins package
     sudo yum install -y fontconfig java-17-openjdk
     sudo yum install -y jenkins
     sudo systemctl daemon-reload
+    sudo systemctl enable --now jenkins
 }
 
 install_terraform() {
     echo "Installing Terraform..."
-    # Add commands to install Terraform here
     sudo yum install -y yum-utils
     sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
     sudo yum -y install terraform
 }
 
 install_docker() {
-    # Removing Previously installed docker Dependancies
-    sudo yum remove docker \
-                    docker-client \
-                    docker-client-latest \
-                    docker-common \
-                    docker-latest \
-                    docker-latest-logrotate \
-                    docker-logrotate \
-                    docker-engine
+    echo "Installing Docker..."
+    # Removing Previously installed Docker dependencies
+    sudo yum remove -y docker \
+                        docker-client \
+                        docker-client-latest \
+                        docker-common \
+                        docker-latest \
+                        docker-latest-logrotate \
+                        docker-logrotate \
+                        docker-engine
 
-    # Adding Docker Repo
+    # Adding Docker repo
     sudo yum install -y yum-utils device-mapper-persistent-data lvm2
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
     # Installing Docker Engine
-    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo systemctl start docker
+    sudo systemctl enable docker
 }
-
-
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 [git|jenkins|ansible|terraform|docker]"
